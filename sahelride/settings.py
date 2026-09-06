7df9c0c2950d3f305745fb07fb8fb76c86edc9d7
Zabel <zabelnabaloum53@gmail.com>
2026-09-06 10:24:42 +0000

    changes 2

--- sahelride/settings.py
+++ sahelride/settings.py
@@ -25,7 +25,7 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'sahelride.urls'
 TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [],
+    'DIRS': [BASE_DIR / 'templates'],
     'APP_DIRS': True,
     'OPTIONS': {'context_processors': [
         'django.template.context_processors.debug',
--- sahelride/urls.py
+++ sahelride/urls.py
@@ -5,12 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('location.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include('location.urls')),  # Vos routes pour la page d'accueil
+]
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    # vos routes existantes
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# Ajout des fichiers statiques et médias (UNIQUEMENT en développement)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
